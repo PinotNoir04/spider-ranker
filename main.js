53392dab479efd26154adc58d7a573b6b2b6f429
PinotNoir04 <rrcemon@gmail.com>
2024-07-02 20:28:45 +0530

    clie report done

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
-import {node:process} from 
+import { crawlPage } from "./spider.js"
 
-const main = () => {
-    return
+const main = async () => {
+    if (process.argv.length != 3){
+        console.log("unexpected input")
+        return
+    }
+    console.log(`Crawling: ${process.argv[2]}`)
+    const pages = await crawlPage(process.argv[2])
+
+    console.log(pages)
 }
+
+main()
--- spider.js
+++ spider.js
@@ -1,5 +1,52 @@
 import {JSDOM} from 'jsdom'
 
+const crawlPage = async (base,curr=base,pages={}) => {
+    const baseUrl = new URL(base)
+    const currUrl = new URL(curr)
+    if (baseUrl.hostname!=currUrl.hostname){
+        return pages
+    }
+    const normCurr = normalizeURL(curr)
+
+    if (pages[normCurr]>0){
+        pages[normCurr]++
+        return pages
+    }
+    pages[normCurr] = 1
+
+    let html=""
+    try{
+        html = await getHtml(curr)
+    } catch(err) {
+        console.log(`${err}`)
+        return pages
+    }
+
+    const nextPages = getUrlsFromHtml(html,base)
+
+    for (let page of nextPages){
+        pages = await crawlPage(baseUrl,page,pages)
+    }
+
+    return pages
+}
+
+const getHtml = async (url) => {
+    let content
+    try{
+        content = await fetch(url)
+    } catch(err){
+        throw new Error(`${err}`)
+    }
+    if (!content.ok){
+        throw new Error(`${content}`)
+    }
+    if (!content.headers.get("content-type").includes("text/html")){
+        throw new Error("invalid type")
+    }
+    return content.text()
+}
+
 const getUrlsFromHtml = (htmlBody, baseUrl) => {
     const dom = new JSDOM(htmlBody)
     const res = []
@@ -25,4 +72,4 @@ const normalizeURL = (url) => {
     return resUrl
 }
 
-export {normalizeURL,getUrlsFromHtml}
+export {normalizeURL,getUrlsFromHtml,crawlPage}
